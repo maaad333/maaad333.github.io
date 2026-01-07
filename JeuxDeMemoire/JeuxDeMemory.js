519e883ef7a40721ca00fce43cbc45a2351bfe94
maaad333 <157286214+maaad333@users.noreply.github.com>
2026-01-07 20:48:36 +0100

    Add files via upload

---
+++ JeuxDeMemoire/JeuxDeMemory.js
@@ -0,0 +1,17 @@
+'use strict';
+
+console.log("hello")
+
+$("img").on({
+    "click": function () {
+        console.log(selection);
+        if (selection == 0) {
+            $(this).hide();
+            id = $(this).data("id");
+            $(id).show();
+            selection = 1;
+            console.log(selection);
+            card1 = $(id).scr;
+        }
+    });
+
